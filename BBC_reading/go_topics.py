5c904655bb084e07d9c52454dee7824a54aac6ee
bateiko <bateiko0713@gmail.com>
2020-03-26 10:51:42 +0200

    crete function to datetime

--- BBC_reading/go_topics.py
+++ BBC_reading/go_topics.py
@@ -1,21 +1,42 @@
 from selenium import webdriver
 import pandas as pd
+import datetime
 
-# driver = webdriver.Chrome(executable_path='/home/bateiko/Downloads/chromedriver_linux64/chromedriver')
-# driver.implicitly_wait(5)
-# driver.maximize_window()
-#
-# main_url = 'https://www.bbc.com/ukrainian/news-51018902'
-#
-# driver.get(main_url)
-# date = driver.find_element_by_xpath('//li[@class="mini-info-list__item"]/div').text
-#
-# print(f"date:\t{date}")
+def go_date_bbc(message):
+    date_dictionary = {
+        "січ" : "01",
+        "лют": "02",
+        "берез": "03",
+        "квіт": "04",
+        "трав": "05",
+        "черв": "06",
+        "лип": "07",
+        "серп": "08",
+        "вер": "09",
+        "жовт": "10",
+        "лист": "11",
+        "груд": "12"
+    }
+    list_of_message = message.split()
 
-t = True
-f = False
+    try:
+        year = int(list_of_message[2].strip())
+        date =  list_of_message[0]
+        current_month = list_of_message[1].strip().lower()
+        for month in date_dictionary.keys():
+            if current_month.startswith(month):
+                return f"{date}/{date_dictionary[month]}/{str(datetime.date.today().year)}"
+    except:
+        return datetime.date.today().strftime("%d/%m/%Y")
 
-if f:
-    print(f"1:\t{t}")
-elif t:
-    print(f"2:\t{f}")
+file_name = "panddas.xlsx"
+columns_my = ["Date", "Author", "Main_Topics", "Topic", "Text"]
+
+df_news = pd.read_excel(file_name, index_col=0)
+
+for coutn, message in enumerate(df_news[columns_my[0]]):
+    # print(f"message:\t{message}\tresult:\t{go_date_bbc(message)}")
+    message = go_date_bbc(message)
+    df_news[columns_my[0]][coutn] = go_date_bbc(message)
+
+print(df_news.head)
