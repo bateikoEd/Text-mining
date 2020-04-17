87b795b58d9c9466a822cba04ee7844b7c9236c0
bateiko <bateiko0713@gmail.com>
2020-04-17 20:24:32 +0300

    scrapping

--- BBC_reading/run_.py
+++ BBC_reading/run_.py
@@ -2,8 +2,8 @@ from class_scrapping import Scrapping
 
 object = Scrapping()
 
-object.len_of_news()
+# object.len_of_news()
 object.nz_scrapping()
 object.bbc_scraping()
-object.len_of_news()
+# object.len_of_news()
 # object.to_all_txt()
