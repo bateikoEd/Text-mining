6b5c8d04cb5a75110236c32ddae5bb9a4b4c7928
bateiko <bateiko0713@gmail.com>
2020-04-28 11:21:43 +0300

    scrapping

--- BBC_reading/run_.py
+++ BBC_reading/run_.py
@@ -2,11 +2,11 @@ from class_scrapping import Scrapping
 
 object = Scrapping()
 
-# start_len = object.len_of_news()
-# object.nz_scrapping()
-# object.bbc_scraping()
-# end_len = object.len_of_news()
-# print(f"count of added news:\t{end_len - start_len}")
+start_len = object.len_of_news()
+object.nz_scrapping()
+object.bbc_scraping()
+end_len = object.len_of_news()
+print(f"count of added news:\t{end_len - start_len}")
 # object.go_to_txt_categorizations()
-object.go_to_txt_one_directory()
+# object.go_to_txt_one_directory()
 
