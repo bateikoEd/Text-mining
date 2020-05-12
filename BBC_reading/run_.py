e36bc82527ec874611642450d2377ad2d6ac6b93
bateiko <bateiko0713@gmail.com>
2020-05-12 22:33:15 +0300

    ok

--- BBC_reading/run_.py
+++ BBC_reading/run_.py
@@ -3,7 +3,7 @@ from class_scrapping import Scrapping
 object = Scrapping()
 
 start_len = object.len_of_news()
-object.nz_scrapping()
+# object.nz_scrapping()
 object.bbc_scraping()
 end_len = object.len_of_news()
 print(f"count of added news:\t{end_len - start_len}")
